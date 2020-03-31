a03808ae36f065b1b2dfa8574dbd47f8c2046e77
PeiroJack <3532926353@qq.com>
2020-04-01 00:18:26 +0800

    gmall后台管理系统:完成平台属性表和平台属性值表的增删改查

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/AttrServiceImpl.java
@@ -0,0 +1,84 @@
+package com.atguigu.gmall.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
+import com.atguigu.gmall.manage.mapper.PmsBaseAttrInfoMapper;
+import com.atguigu.gmall.manage.mapper.PmsBaseAttrValueMapper;
+import com.atguigu.gmall.service.AttrService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.persistence.Id;
+import java.util.List;
+
+@Service
+public class AttrServiceImpl implements AttrService {
+
+    @Autowired
+    PmsBaseAttrInfoMapper pmsBaseAttrInfoMapper;
+
+    @Autowired
+    PmsBaseAttrValueMapper pmsBaseAttrValueMapper;
+
+    @Override
+    public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id) {
+        PmsBaseAttrInfo pmsBaseAttrInfo = new PmsBaseAttrInfo();
+        pmsBaseAttrInfo.setCatalog3Id(catalog3Id);
+        List<PmsBaseAttrInfo> pmsBaseAttrInfos = pmsBaseAttrInfoMapper.select(pmsBaseAttrInfo);
+        return pmsBaseAttrInfos;
+    }
+
+    @Override
+    public String saveAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo) {
+        String id = pmsBaseAttrInfo.getId();
+        if(StringUtils.isBlank(id)){
+            //id为空，为保存操作
+            pmsBaseAttrInfoMapper.insertSelective(pmsBaseAttrInfo);//添加属性名称表
+
+            List<PmsBaseAttrValue> attrValueList = pmsBaseAttrInfo.getAttrValueList();//添加属性值表
+            for (PmsBaseAttrValue pmsBaseAttrValue : attrValueList) {
+                pmsBaseAttrValue.setAttrId(pmsBaseAttrInfo.getId());
+                pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValue);
+            }
+        }else{
+            //修改操作
+            //修改平台属性
+            Example example = new Example(PmsBaseAttrInfo.class);
+            example.createCriteria().andEqualTo("id",id);
+            pmsBaseAttrInfoMapper.updateByExample(pmsBaseAttrInfo,example);
+
+            //修改平台属性值
+            //删除对应的平台属性值
+            PmsBaseAttrValue pmsBaseAttrValueDel = new PmsBaseAttrValue();
+            pmsBaseAttrValueDel.setAttrId(id);
+            pmsBaseAttrValueMapper.delete(pmsBaseAttrValueDel);
+
+            //得到要修改的平台属性值
+            List<PmsBaseAttrValue> pmsBaseAttrValues = pmsBaseAttrInfo.getAttrValueList();
+            //增加新的平台属性值
+            for (PmsBaseAttrValue pmsBaseAttrValue : pmsBaseAttrValues) {
+                pmsBaseAttrValue.setAttrId(id);
+                pmsBaseAttrValueMapper.insert(pmsBaseAttrValue);
+            }
+
+
+        }
+
+
+        return "success";
+    }
+
+
+
+    @Override
+    public List<PmsBaseAttrValue> getAttrValueList(String attrId) {
+        PmsBaseAttrValue pmsBaseAttrValue = new PmsBaseAttrValue();
+        pmsBaseAttrValue.setAttrId(attrId);
+        List<PmsBaseAttrValue> pmsBaseAttrValues = pmsBaseAttrValueMapper.select(pmsBaseAttrValue);
+        return pmsBaseAttrValues;
+    }
+
+
+}
